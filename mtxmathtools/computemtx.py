77fafde0bb05e53c305b56a9d4c6b3162523aaa4
sivepanda <sivenpanda@icloud.com>
2024-12-12 13:01:13 -0500

    Ops in matrix for code readability

--- mtxmathtools/computemtx.py
+++ mtxmathtools/computemtx.py
@@ -1,5 +1,23 @@
 from sympy import *
 
+avail_operations = {
+    "1" : "Reduced Row Echelon Form",
+    "2" : "Null Space",              
+    "3" : "Column Space",              
+    "4" : "Transpose",              
+    "5" : "Left Null Space",              
+    "6" : "Transpose Row Echelon Form",              
+    "7" : "Determinant",              
+    "8" : "Eigenvectors",              
+    "9" : "Diagnonalize",              
+    "10" : "Normalize Columns",             
+    "11" : "Multiply with Another Matrix",              
+    "12" : "Multiply with Transpose (AAT)",              
+    "13" : "Multiply with Transpose (ATA)",              
+    "14" : "Singular Value Decomposition",              
+    "15" : "Gram-Schmidt (Columns are Vectors)"
+}
+
 def inputMatrix():
     print("Input the matrix line by line. Press enter when you are done.\n")
     matrix = []
@@ -31,34 +49,27 @@ def main():
     idenn = eye ( shape(a)[0] )
 
     print("\n\nSelect the operation you would like to compute. Type the number of your choice.\nIf you would like to compute multiple things, enter both numbers delineated by a space.\n")
-    op = input("1\tReduced Row Echelon Form\n2\tNull Space\n3\tColumn Space\n4\tTranspose\n5\tLeft Null Space\n6\tTranspose Row Echelon Form\n7\tDeterminant\n8\tEigenvectors\n9\tDiagnonalize\n10\tNormalize Columns\n11\tMultiply with Another Matrix\n12\tMultiply with Transpose (AAT)\n13\tMultiply with Transpose (ATA)\n14\tSingular Value Decomposition\n15\tGram-Schmidt (Columns are Vectors)\n")
+    op = input("".join(f"{key}\t{val}\n" for key, val in avail_operations.items()))
     print("\n")
 
     for operation in op.split(" "):
         out = "Invalid Operation."
+        print(avail_operations[operation] + "========")
         if (operation == "1"):
-            print("Reduced Row Echelon Form====")
             out = a.rref(pivots=False)
         if (operation == "2"):
-            print("Null Space====")
             out = a.nullspace()
         if (operation == "3"):
-            print("Column Space====")
             out = a.columnspace()
         if (operation == "4"):
-            print("Transpose====")
             out = a_t
         if (operation == "5"):
-            print("Left Null Space====")
             out = a_t.nullspace()
         if (operation == "6"):
-            print("Transpose Row Echelon Form====")
             out = a_t.rref(pivots=False)
         if (operation == "7"):
-            print("Determinant====")
             out = a.det()
         if (operation == "8"):
-            print("Eigenvectors====")
             eig =input("If you would like to calculate the eigenvectors for a particular value, enter it. If not, hit enter.\n")
             print("\n")
             if(len(eig) == 0):
@@ -67,14 +78,12 @@ def main():
                 eig = sympify(eig)
                 out = ( a - ( eig * idenn ) ).nullspace()
         if (operation == "9"):
-            print("Diagonalize====")
             c, d = a.diagonalize()
             print("C===")
             pprint(c)
             print("D===")
             out = d
         if (operation == "10"):
-            print("Normalize Columns====")
             outcols = []
             cols = [a.col(i) for i in range(a.cols)]
             for col in cols:
@@ -82,7 +91,6 @@ def main():
             out = Matrix.hstack(*outcols)
 
         if (operation == "11"):
-            print("Multiply Matrices====")
             correctMatrix = "no"
             while correctMatrix == "no":
                 b = inputMatrix()
@@ -93,19 +101,16 @@ def main():
                 out = a * b
 
         if (operation == "12"):
-            print("Multiply by Transpose (AAT)====")
             out = a * a_t
 
         if (operation == "13"):
-            print("Multiply by Transpose (ATA)====")
             out = a_t * a
 
         if (operation == "14"):
-            print("Singular Value Decomposition====")
             out = a.singular_value_decomposition() 
 
         if (operation == "15"):
-            print("Gram-Schmidt====")
+            print("WARNING: Takes a naive approach, uses first col as u1, regardless of ease of calculation")
             outcols = []
             cols = [a.col(i) for i in range(a.cols)]
             outcols.append(cols.pop(0))
